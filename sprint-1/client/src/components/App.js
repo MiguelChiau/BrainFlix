a303e8294185f1b48d740a06413dbe21873989bb
Miguel Chiau <chiau.miguel@gmail.com>
2019-07-24 16:36:25 -0700

    hero component

--- sprint-1/client/src/components/App.js
+++ sprint-1/client/src/components/App.js
@@ -1,33 +1,16 @@
 import React from "react";
 import Header from "./Header.js";
+import Hero from "./Hero.js";
+
 import "../styles/app.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
---
+++ sprint-1/client/src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Background from "../assets/Images/video-list-0.jpg";
+// import Video from "../assets/Video/BrainStation-Sample-Video.mp4";
+
+function Hero() {
+  return (
+    <div className="hero-background">
+      <img src={Background} alt="" />
+    </div>
+  );
+}
+
+export default Hero;
