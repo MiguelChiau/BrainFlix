5bf7bff246f3c2ba1fcc6ae2629d37489cf0e371
Miguel Chiau <chiau.miguel@gmail.com>
2019-07-25 13:39:33 -0700

    responsive hero

--- sprint-1/client/src/components/Hero.js
+++ sprint-1/client/src/components/Hero.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Background from "../assets/Images/video-list-0.jpg";
 // import Fullscreen from "../assets/Icons/PNG/Icon-fullscreen.png";
-// import Volume from "../assets/Icons/PNG/";
+// import Volume from "../assets/Icons/PNG/Icon-volume.png";
+// import Play from "../assets/Icons/PNG/Icon-play.png";
 
 function Hero() {
   return (
     <div className="hero-background">
-      <video controls poster={Background} />
+      <div className="hero-background__video">
+        <video controls poster={Background} />
+      </div>
     </div>
   );
 }
