89aee70fc3d40b303fe39f44d38a98dae18f2e77
Miguel Chiau <chiau.miguel@gmail.com>
2019-07-24 17:47:59 -0700

    video hero

--- sprint-1/client/src/components/Hero.js
+++ sprint-1/client/src/components/Hero.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Background from "../assets/Images/video-list-0.jpg";
-// import Video from "../assets/Video/BrainStation-Sample-Video.mp4";
+// import Fullscreen from "../assets/Icons/PNG/Icon-fullscreen.png";
+// import Volume from "../assets/Icons/PNG/";
 
 function Hero() {
   return (
     <div className="hero-background">
-      <img src={Background} alt="" />
+      <video controls poster={Background} />
     </div>
   );
 }
