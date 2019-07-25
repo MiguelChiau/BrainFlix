f3de63071bbac7793b7ff8feca9d0fac1bfad1d9
Miguel Chiau <chiau.miguel@gmail.com>
2019-07-25 16:23:15 -0700

    video controls: test

--- sprint-1/client/src/components/Hero.js
+++ sprint-1/client/src/components/Hero.js
@@ -1,13 +1,33 @@
 import React from "react";
 import Background from "../assets/Images/video-list-0.jpg";
-// import Fullscreen from "../assets/Icons/PNG/Icon-fullscreen.png";
-// import Volume from "../assets/Icons/PNG/Icon-volume.png";
-// import Play from "../assets/Icons/PNG/Icon-play.png";
+import Play from "../assets/Icons/PNG/Icon-play.png";
+import Fullscreen from "../assets/Icons/PNG/Icon-fullscreen.png";
+import Volume from "../assets/Icons/PNG/Icon-volume.png";
 
 function Hero() {
   return (
-    <div className="hero-background">
-      <video poster={Background} />
+    <div>
+      <div className="hero-background">
+        <video poster={Background} />
+
+        <div className="controls">
+          <button className="controls-play">
+            <img src={Play} alt="" />
+          </button>
+
+          <div className="controls-range" />
+
+          <div className="controls-buttons">
+            <button>
+              <img src={Fullscreen} alt="" />
+            </button>
+
+            <button>
+              <img src={Volume} alt="" />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
