cc8cfd51e1094504bceaaf6ebafcdcb166ddf650
Miguel Chiau <chiau.miguel@gmail.com>
2019-07-25 23:04:31 -0700

    responsive tablet: title section

--- sprint-1/client/src/components/Title.js
+++ sprint-1/client/src/components/Title.js
@@ -4,23 +4,25 @@ import Likes from "../assets/Icons/SVG/Icon-likes.svg";
 
 function Title() {
   return (
-    <div className="container-title">
+    <div className="container-all">
       <h1>BMX Rampage: 2018 Highlights</h1>
 
-      <div className="container-author">
-        <h2>By Red Cow</h2>
-        <h5>12/18/2018</h5>
-      </div>
-
-      <div className="container">
-        <div className="container-views">
-          <img src={Views} alt="" />
-          <h5>1,001,023</h5>
+      <div className="container-authorAndIcons">
+        <div className="container-author">
+          <h2>By Red Cow</h2>
+          <h5>12/18/2018</h5>
         </div>
 
-        <div className="container-likes">
-          <img src={Likes} alt="" />
-          <h5>110,985</h5>
+        <div className="container-icons">
+          <div className="container-icons__views">
+            <img src={Views} alt="" />
+            <h5>1,001,023</h5>
+          </div>
+
+          <div className="container-icons__likes">
+            <img src={Likes} alt="" />
+            <h5>110,985</h5>
+          </div>
         </div>
       </div>
     </div>
