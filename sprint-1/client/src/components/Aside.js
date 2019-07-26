a7fe8d50a1176101b5b0bf435d03a890201c8f77
Miguel Chiau <chiau.miguel@gmail.com>
2019-07-26 12:11:52 -0700

    next video

--- sprint-1/client/src/components/Aside.js
+++ sprint-1/client/src/components/Aside.js
@@ -11,15 +11,87 @@ import Video8 from "../assets/Images/video-list-8.jpg";
 function Aside() {
   return (
     <div className="container-videos">
-      <h2>Next Videos</h2>
-      <video poster={Video1} />
-      <video poster={Video2} />
-      <video poster={Video3} />
-      <video poster={Video4} />
-      <video poster={Video5} />
-      <video poster={Video6} />
-      <video poster={Video7} />
-      <video poster={Video8} />
+      <h2>NEXT VIDEO</h2>
+
+      <div className="video-one">
+        <video poster={Video1} />
+        <div>
+          <p className="video-one__title">
+            Become a Travel Pro In One Easy Lesson...
+          </p>
+          <p className="video-one__author">Scotty Cranmer </p>
+        </div>
+      </div>
+
+      <div className="video-two">
+        <video poster={Video2} />
+        <div>
+          <p className="video-one__title">
+            Become a Travel Pro In One Easy Lesson...
+          </p>
+          <p className="video-one__author">Scotty Cranmer </p>
+        </div>
+      </div>
+
+      <div className="video-three">
+        <video poster={Video3} />
+        <div>
+          <p className="video-one__title">
+            Become a Travel Pro In One Easy Lesson...
+          </p>
+          <p className="video-one__author">Scotty Cranmer </p>
+        </div>
+      </div>
+
+      <div className="video-four">
+        <video poster={Video4} />
+        <div>
+          <p className="video-one__title">
+            Become a Travel Pro In One Easy Lesson...
+          </p>
+          <p className="video-one__author">Scotty Cranmer </p>
+        </div>
+      </div>
+
+      <div className="video-five">
+        <video poster={Video5} />
+        <div>
+          <p className="video-one__title">
+            Become a Travel Pro In One Easy Lesson...
+          </p>
+          <p className="video-one__author">Scotty Cranmer </p>
+        </div>
+      </div>
+
+      <div className="video-six">
+        <video poster={Video6} />
+        <div>
+          <p className="video-one__title">
+            Become a Travel Pro In One Easy Lesson...
+          </p>
+          <p className="video-one__author">Scotty Cranmer </p>
+        </div>
+      </div>
+
+      <div className="video-seven">
+        <video poster={Video7} />
+        <div>
+          <p className="video-one__title">
+            Become a Travel Pro In One Easy Lesson...
+          </p>
+          <p className="video-one__author">Scotty Cranmer </p>
+        </div>
+      </div>
+
+      <div className="video-eight">
+        <video poster={Video8} />
+        <div>
+          <p className="video-one__title">
+            Become a Travel Pro In One Easy Lesson...
+          </p>
+          <p className="video-one__author">Scotty Cranmer </p>
+        </div>
+      </div>
     </div>
   );
 }
