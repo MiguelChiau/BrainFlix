e861581c9c4e3fdb57d66ff924f3a3dd47728a9b
Miguel Chiau <chiau.miguel@gmail.com>
2019-07-30 17:07:33 -0700

    link to header logo

--- sprint-2/client/src/components/App.js
+++ sprint-2/client/src/components/App.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./Header.js";
 import Hero from "./Hero.js";
 import MainBody from "./MainBody.js";
+import { Route, Switch } from "react-router-dom";
 
 import "../styles/app.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Hero />
-      <MainBody />
-    </div>
-  );
+// const myKey = `d9ee6782-fc1c-4908-9ee6-d878e091f619`;
+
+// const URL = `https://project-2-api.herokuapp.com`;
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Header} />
+        </Switch>
+
+        {/* <Header /> */}
+        <Hero />
+        <MainBody />
+      </div>
+    );
+  }
 }
 
 export default App;
--- sprint-2/client/src/components/Header.js
+++ sprint-2/client/src/components/Header.js
@@ -3,12 +3,15 @@ import Logo from "../assets/Logo/Logo-brainflix.svg";
 import SearchIcon from "../assets/Icons/SVG/Icon-search.svg";
 import UploadIcon from "../assets/Icons/SVG/Icon-upload.svg";
 import UserProfile from "../assets/Images/Mohan-muruge.jpg";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header-container">
       <div className="header-container__logo">
-        <img src={Logo} alt="" />
+        <Link to="/">
+          <img src={Logo} alt="" />
+        </Link>
       </div>
 
       <div className="header-search">
