401d0a880e5173596e840d709f8203e29b8da473
Miguel Chiau <chiau.miguel@gmail.com>
2019-08-05 14:57:01 -0700

    comments section updates

--- sprint-2/client/src/components/AddComment.js
+++ sprint-2/client/src/components/AddComment.js
@@ -24,28 +24,30 @@ export default class AddComment extends Component {
   render() {
     return (
       <div className="container-All">
-        <p className="container-title">3 Comments</p>
+        <p className="container-header">3 Comments</p>
         <form className="container" onSubmit={this.handleSubmit}>
-          <div className="container-avatar">
-            <img src={Avatar} alt="" />
-          </div>
+          <div className="container-big">
+            <div className="container-avatar">
+              <img src={Avatar} alt="" />
+            </div>
 
-          <div className="container-comment">
-            <div className="container-comment__input">
-              <div className="container-comment__title">
-                <label>JOIN THE CONVERSATION</label>
+            <div className="container-comment">
+              <div className="container-comment__input">
+                <div className="container-comment__title">
+                  <label>JOIN THE CONVERSATION</label>
 
-                <textarea
-                  rows="4"
-                  rows="4"
-                  cols="50"
-                  onChange={this.handleChange}
-                  name="comment"
-                  placeholder="That was easily the most spectacular BMX moment ever."
-                />
-              </div>
-              <div>
-                <button className="container-comment__btn">COMMENT</button>
+                  <textarea
+                    rows="4"
+                    rows="4"
+                    cols="50"
+                    onChange={this.handleChange}
+                    name="comment"
+                    placeholder="That was easily the most spectacular BMX moment ever."
+                  />
+                </div>
+                <div>
+                  <button className="container-comment__btn">COMMENT</button>
+                </div>
               </div>
             </div>
           </div>
